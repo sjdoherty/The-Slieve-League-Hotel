9a28136adfaa63f75ceaacc41543ee5a6b5ccdb2
sjdoherty <92801020+sjdoherty@users.noreply.github.com>
2021-12-03 14:07:50 +0000

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+/*window.onload=function()  //executes when the page finishes loading
+{
+	setTimeout(func1, 6000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
+	
+};
+function func1()
+{
+	document.getElementById("my_div").className="show";
+}
