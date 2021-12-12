ac7218092956ecde1b4dd5fc77a60cabfd25317b
sjdoherty <92801020+sjdoherty@users.noreply.github.com>
2021-12-12 19:33:41 +0000

    update

--- script.js
+++ script.js
@@ -1,9 +1,76 @@
-/*window.onload=function()  //executes when the page finishes loading
-{
-	setTimeout(func1, 6000);  //sets a timer which calls function func1 after 2,000 milliseconds = 2 secs.
-	
-};
-function func1()
-{
-	document.getElementById("my_div").className="show";
-}
+
+ $(document).ready(function(){
+    $("#date").datepicker();
+
+    $('#myForm').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            firstname: {
+                required: true
+            },
+            lastname: {
+                required: true
+            },
+                // countries: {
+                // 	required: true
+                // },
+            date: {
+                required: true
+            },
+                // terms: {
+                // 	required: true
+                // }
+            people: {
+                required: true
+            },
+            rooms: {
+                required: true
+            },
+            roomSize: {
+                required: true
+            },
+            nights: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                email: "Please enter a Valid Email"
+            },
+            firstname: {
+                required: "Enter your FirstName"
+            },
+            lastname: {
+                required: "Enter your LastName"
+            },
+               
+            date: {
+                required: "Enter your Date of Birth",
+                validDate: true
+            },
+                
+            people: {
+                required: "Number of people required"
+            },
+            rooms: {
+                required: "Number of rooms required"
+            },
+            roomSize: {
+                required: "Room size required"
+            },
+            nights: {
+                required: "Number of nights required"
+            }
+        },
+        success: function(label){
+            label.addClass("valid").text("Ok!");
+        },
+        submitHandler: function(){
+            $("#myForm").hide();
+            $("#successMsg").html("<p>Booking Received. Your room is now reserved. We will send you a confirmation email shortly.</p>>");
+        }
+    });
+});
